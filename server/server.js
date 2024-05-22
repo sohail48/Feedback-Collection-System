f6145daa582e9dd406eed2f58386a179250dd2c3
Naeem Navjivan <n.navjivan@qvitec.in>
2024-05-22 17:14:17 +0530

    add data to mongodb

--- server/server.js
+++ server/server.js
@@ -3,9 +3,30 @@ const cors = require('cors')
 const express = require('express');
 const bodyParser = require('body-parser');
 const multer = require('multer');
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/mydb', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected...'))
+  .catch(err => console.log(err));
+  const formDataSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email: String,
+    contact: String,
+    gender: String,
+    selectedOption: String,
+    subjects: String,
+    resumeUrl: String,
+    url: String,
+    about: String
+  });
+  
+  // Create a model
+  const FormData = mongoose.model('FormData', formDataSchema);
 // Middleware to parse JSON bodies
 app.use(bodyParser.json());
 app.use(cors())
@@ -26,6 +47,22 @@ app.post('/submit', upload.single('resume'), (req, res) => {
     url, 
     about 
   } = req.body;
+  const newFormData = new FormData({
+    firstName,
+    lastName,
+    email,
+    contact,
+    gender,
+    selectedOption,
+    subjects,
+    resumeUrl: req.file ? `uploads/${req.file.originalname}` : '',
+    url,
+    about
+  });
+
+  newFormData.save()
+    .then(() => res.status(200).send('Form data received and saved to MongoDB.'))
+    .catch(err => res.status(500).send('Error saving data to MongoDB'));
 
   console.log('First Name:', firstName);
   console.log('Last Name:', lastName);
